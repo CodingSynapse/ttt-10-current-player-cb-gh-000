5d565f3427cd6ea5fed2f3713acb9a24f42ba9a1
Micah ortega <codingsynapse@gmail.com>
2019-11-06 07:00:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,7 @@
 def turn_count(board)
   turns = 1
   board.each do |pos|
-    position = pos
-  end
-    if position == "X" || positon == "O"
+    if board == "X" || board == "O"
       turns += 1
     end
 end
